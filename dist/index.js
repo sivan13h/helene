9d90f016cae76044ae4b1b10cd1e8b903e1d1ec5
Sivan Hart <sivan13h@gmail.com>
2021-04-30 16:34:05 +0200

    add back arrow

--- dist/index.js
+++ dist/index.js
@@ -54,30 +54,3 @@ toggler.addEventListener("click", () => {
 //     mainNav.classList.add("top");
 //   }
 // };
-
-getQuote();
-
-$(document).ready(function () {
-  $("#getMessage").on("click", function () {
-    getQuote();
-  });
-});
-
-function getQuote() {
-  $.getJSON(
-    "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
-    function (data) {
-      $("#quote").html(data.quoteText);
-      $("#author").html(data.quoteAuthor);
-      $("#tweetQuote")
-        .attr(
-          "href",
-          "https://twitter.com/intent/tweet?text=" +
-            data.quoteText +
-            "- " +
-            data.quoteAuthor
-        )
-        .attr("target", "_blank");
-    }
-  );
-}
