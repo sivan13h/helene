84b1dbf177006915ec83927be3a31536cce0b685
Sivan Hart <sivan13h@gmail.com>
2021-04-30 13:28:52 +0200

    first changes after jojo sent buttons

--- dist/index.js
+++ dist/index.js
@@ -21,7 +21,7 @@ function checkSlide() {
   sliderImages.forEach((sliderImage) => {
     // half way through the image
     const slideInAt =
-      window.scrollY + window.innerHeight - sliderImage.height / 2;
+      window.scrollY + window.innerHeight - sliderImage.height / 2 - 20;
     // bottom of the image
     const imageBottom = sliderImage.offsetTop + sliderImage.height;
     const isHalfShown = slideInAt > sliderImage.offsetTop;
@@ -46,120 +46,38 @@ toggler.addEventListener("click", () => {
   smallNav.classList.toggle("show");
 });
 
-window.onscroll = function () {
-  if (window.pageYOffset > 300) {
-    mainNav.classList.remove("top");
-  } else {
-    mainNav.classList.add("top");
-  }
-};
+// window.onscroll = function () {
+//   console.log(window.pageYOffset);
+//   if (window.pageYOffset > 300) {
+//     mainNav.classList.remove("top");
+//   } else {
+//     mainNav.classList.add("top");
+//   }
+// };
 
-// testimonials
-// vars
-("use strict");
-var testim = document.getElementById("testim"),
-  testimDots = Array.prototype.slice.call(
-    document.getElementById("testim-dots").children
-  ),
-  testimContent = Array.prototype.slice.call(
-    document.getElementById("testim-content").children
-  ),
-  testimLeftArrow = document.getElementById("left-arrow"),
-  testimRightArrow = document.getElementById("right-arrow"),
-  testimSpeed = 4500,
-  currentSlide = 0,
-  currentActive = 0,
-  testimTimer,
-  touchStartPos,
-  touchEndPos,
-  touchPosDiff,
-  ignoreTouch = 30;
-window.onload = function () {
-  // Testim Script
-  function playSlide(slide) {
-    for (var k = 0; k < testimDots.length; k++) {
-      testimContent[k].classList.remove("active");
-      testimContent[k].classList.remove("inactive");
-      testimDots[k].classList.remove("active");
-    }
+getQuote();
 
-    if (slide < 0) {
-      slide = currentSlide = testimContent.length - 1;
-    }
-
-    if (slide > testimContent.length - 1) {
-      slide = currentSlide = 0;
-    }
-
-    if (currentActive != currentSlide) {
-      testimContent[currentActive].classList.add("inactive");
-    }
-    testimContent[slide].classList.add("active");
-    testimDots[slide].classList.add("active");
-
-    currentActive = currentSlide;
-
-    clearTimeout(testimTimer);
-    testimTimer = setTimeout(function () {
-      playSlide((currentSlide += 1));
-    }, testimSpeed);
-  }
-
-  testimLeftArrow.addEventListener("click", function () {
-    playSlide((currentSlide -= 1));
+$(document).ready(function () {
+  $("#getMessage").on("click", function () {
+    getQuote();
   });
+});
 
-  testimRightArrow.addEventListener("click", function () {
-    playSlide((currentSlide += 1));
-  });
-
-  for (var l = 0; l < testimDots.length; l++) {
-    testimDots[l].addEventListener("click", function () {
-      playSlide((currentSlide = testimDots.indexOf(this)));
-    });
-  }
-
-  playSlide(currentSlide);
-
-  // keyboard shortcuts
-  document.addEventListener("keyup", function (e) {
-    switch (e.keyCode) {
-      case 37:
-        testimLeftArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      default:
-        break;
+function getQuote() {
+  $.getJSON(
+    "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+    function (data) {
+      $("#quote").html(data.quoteText);
+      $("#author").html(data.quoteAuthor);
+      $("#tweetQuote")
+        .attr(
+          "href",
+          "https://twitter.com/intent/tweet?text=" +
+            data.quoteText +
+            "- " +
+            data.quoteAuthor
+        )
+        .attr("target", "_blank");
     }
-  });
-
-  testim.addEventListener("touchstart", function (e) {
-    touchStartPos = e.changedTouches[0].clientX;
-  });
-
-  testim.addEventListener("touchend", function (e) {
-    touchEndPos = e.changedTouches[0].clientX;
-
-    touchPosDiff = touchStartPos - touchEndPos;
-
-    console.log(touchPosDiff);
-    console.log(touchStartPos);
-    console.log(touchEndPos);
-
-    if (touchPosDiff > 0 + ignoreTouch) {
-      testimLeftArrow.click();
-    } else if (touchPosDiff < 0 - ignoreTouch) {
-      testimRightArrow.click();
-    } else {
-      return;
-    }
-  });
-};
+  );
+}
